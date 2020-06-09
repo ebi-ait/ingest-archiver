cac94581ae19678db04d710e3fdd1cc0cd72114f
Alegria Aclan <aaclan@ebi.ac.uk>
2020-06-09 11:55:36 +0100

    Added an endpoint to get all validation result

--- app.py
+++ app.py
@@ -33,9 +33,17 @@ def index():
 @app.route("/archiveSubmissions", methods=['POST'])
 def archive():
     data = request.get_json()
+
     submission_uuid = data.get('submission_uuid')
     exclude_types = data.get('exclude_types')
     alias_prefix = data.get('alias_prefix')
+
+    if not submission_uuid:
+        error = {
+            'message': f'You must supply the parameter submission_uuid referring to an Ingest submission envelope uuid.'
+        }
+        return response_json(HTTPStatus.BAD_REQUEST, error)
+
     ingest_api = IngestAPI(config.INGEST_API_URL)
     archiver = IngestArchiver(ingest_api=ingest_api,
                               dsp_api=DataSubmissionPortal(config.DSP_API_URL),
@@ -86,6 +94,15 @@ def get_validation_errors(archive_submission_uuid: str):
     submission = ArchiveSubmission(dsp_api=dsp_api, dsp_submission_url=submission_url)
     validation_errors = submission.get_validation_error_report()
     return jsonify(validation_errors)
+
+
+@app.route('/archiveSubmissions/<archive_submission_uuid>/validationResult')
+def get_validation_result(archive_submission_uuid: str):
+    dsp_api = DataSubmissionPortal(config.DSP_API_URL)
+    submission_url = dsp_api.get_submission_url(archive_submission_uuid)
+    submission = ArchiveSubmission(dsp_api=dsp_api, dsp_submission_url=submission_url)
+    result = submission.get_all_validation_result_details()
+    return jsonify(result)
 
 
 @app.route('/archiveSubmissions/<archive_submission_uuid>/blockers')
