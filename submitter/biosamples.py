fa301a521b96c10af4bd4b632f471c29a0a62f3c
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-04-30 17:24:48 +0100

    BioSamples Submitter

---
+++ submitter/biosamples.py
@@ -0,0 +1,32 @@
+from converter.biosamples import BioSamplesConverter
+from submission_broker.submission.submission import Submission, Entity
+from submission_broker.services.biosamples import BioSamples, AapClient
+
+
+class BioSamplesSubmitter:
+    def __init__(self, aap_client: AapClient, biosamples_url: str, domain: str):
+        self.__biosamples = BioSamples(aap_client, biosamples_url)
+        self.__converter = BioSamplesConverter(domain)
+
+    def send_all_samples(self, submission: Submission):
+        project_release_date = self.__get_project_release_date_from_submission(submission)
+        for sample in submission.get_entities('biomaterials'):
+            self.send_sample(sample, project_release_date)
+
+    def send_sample(self, sample: Entity, release_date: str = None):
+        accession = sample.get_accession('BioSamples')
+        biosample = self.__converter.convert(sample.attributes, release_date, accession)
+        try:
+            response = self.__biosamples.send_sample(biosample)
+            if 'accession' in response and not accession:
+                sample.add_accession('BioSamples', response['accession'])
+        except Exception as e:
+            error_msg = f'BioSamples Error: {e}'
+            sample.add_error('content.biomaterial_core.biosamples_accession', error_msg)
+
+    @staticmethod
+    def __get_project_release_date_from_submission(submission: Submission) -> str:
+        projects = submission.get_entities('projects')
+        for project in projects:
+            if 'releaseDate' in project.attributes:
+                return project.attributes['releaseDate']
