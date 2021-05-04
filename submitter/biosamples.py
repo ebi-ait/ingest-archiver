cb8298ad4fcb82d58eee0f042176ea95d7eef2a2
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-05-04 17:53:11 +0100

    return values and better init params for uni testing.

--- submitter/biosamples.py
+++ submitter/biosamples.py
@@ -4,25 +4,31 @@ from submission_broker.services.biosamples import BioSamples, AapClient
 
 
 class BioSamplesSubmitter:
-    def __init__(self, aap_client: AapClient, biosamples_url: str, domain: str):
-        self.__biosamples = BioSamples(aap_client, biosamples_url)
-        self.__converter = BioSamplesConverter(domain)
+    def __init__(self, biosamples: BioSamples, converter: BioSamplesConverter):
+        self.__biosamples = biosamples
+        self.__converter = converter
 
-    def send_all_samples(self, submission: Submission):
+    def send_all_samples(self, submission: Submission) -> dict:
+        response = {}
         project_release_date = self.__get_project_release_date_from_submission(submission)
         for sample in submission.get_entities('biomaterials'):
-            self.send_sample(sample, project_release_date)
+            result = self.send_sample(sample, project_release_date)
+            response.setdefault(result, []).append(sample)
+        return response
 
-    def send_sample(self, sample: Entity, release_date: str = None):
+    def send_sample(self, sample: Entity, release_date: str = None) -> str:
         accession = sample.get_accession('BioSamples')
         biosample = self.__converter.convert(sample.attributes, release_date, accession)
         try:
             response = self.__biosamples.send_sample(biosample)
             if 'accession' in response and not accession:
                 sample.add_accession('BioSamples', response['accession'])
+                return f"CREATED"
+            return f"UPDATED"
         except Exception as e:
             error_msg = f'BioSamples Error: {e}'
             sample.add_error('content.biomaterial_core.biosamples_accession', error_msg)
+            return "ERRORED"
 
     @staticmethod
     def __get_project_release_date_from_submission(submission: Submission) -> str:
