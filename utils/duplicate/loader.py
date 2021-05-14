a5063e2cfb74a0b8c6e7b43ad60c44ed9150d0b3
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2021-05-14 11:35:44 +0100

    Change permissions of HcaLoader methods for more effective testing by DuplicateLoader

--- utils/duplicate/loader.py
+++ utils/duplicate/loader.py
@@ -3,19 +3,14 @@ from .submission import DuplicateSubmission
 
 
 class DuplicateLoader(HcaLoader):
-    def duplicate_project(self, project_uuid: str) -> DuplicateSubmission:
-        hca_submission = DuplicateSubmission()
-        project_type = 'projects'
-        project = self.__map_entity(hca_submission, project_type, project_uuid)
-        submission_type = 'submissionEnvelopes'
-        self.__map_link_type_to_link_names(hca_submission, project, submission_type)
-        for submission_entity in hca_submission.get_entities(submission_type):
-            self.__map_submission_manifests(hca_submission, submission_entity)
-        return hca_submission
+    def duplicate_project(self, project_uuid: str, submission: DuplicateSubmission = None) -> DuplicateSubmission:
+        if not submission:
+            submission = DuplicateSubmission()
+        self._map_project(submission, project_uuid)
+        return submission
 
-    def duplicate_submission(self, submission_uuid: str) -> DuplicateSubmission:
-        hca_submission = DuplicateSubmission()
-        submission_type = 'submissionEnvelopes'
-        submission_entity = self.__map_entity(hca_submission, submission_type, submission_uuid)
-        self.__map_submission_manifests(hca_submission, submission_entity)
-        return hca_submission
+    def duplicate_submission(self, submission_uuid: str, submission: DuplicateSubmission = None) -> DuplicateSubmission:
+        if not submission:
+            submission = DuplicateSubmission()
+        self._map_submission(submission, submission_uuid)
+        return submission
