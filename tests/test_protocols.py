6fb65a9a4ba7a86f312a26df26a97f94a1929a68
Alegria Aclan <aaclan@ebi.ac.uk>
2021-07-08 18:24:14 +0100

    Optmised imports

--- tests/test_protocols.py
+++ tests/test_protocols.py
@@ -1,10 +1,8 @@
 import json
 from unittest import TestCase
 from unittest.mock import Mock
-
-import config
 from utils import protocols
-from api.ontology import quote, get_json, get_all, OntologyAPI
+from api.ontology import OntologyAPI
 
 
 class TestProtocols(TestCase):
