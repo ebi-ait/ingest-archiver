004e624a033568617df17e87ef8140c70b3b553b
Alegria Aclan <aaclan@ebi.ac.uk>
2021-08-02 08:13:59 +0800

    Make ftp parent dir an optional argument

--- ena/submit_10x_fastq_files.py
+++ ena/submit_10x_fastq_files.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Submits sequencing run entities to ENA')
     parser.add_argument('ingest_submission_uuid', type=str, help='Ingest submission UUID')
     parser.add_argument('md5_file', type=str, help='Filename containing md5 of files for the Ingest submission')
-    parser.add_argument('ftp_dir', type=str, help='')
+    parser.add_argument('--ftp_dir', type=str, required=False, help='Directory of files in the FTP upload area.')
 
     args = parser.parse_args()
 
@@ -20,9 +20,8 @@ if __name__ == "__main__":
     ena_api = EnaApi(ingest_api)
 
     manifest_ids = ingest_api.get_manifest_ids_from_submission(args.ingest_submission_uuid)
-    files = ena_api.create_xml_files(manifest_ids, args.md5_file)
+    files = ena_api.create_xml_files(manifest_ids, args.md5_file, args.ftp_dir)
 
     result_xml_tree = ena_api.post_files(files)
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     write_xml(result_xml_tree, f'receipt_{timestamp}.xml')
-
