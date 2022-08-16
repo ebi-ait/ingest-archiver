c4b3aabb392459d800969a5d8820a1de2779380e
Karoly Erdos <karoly@ebi.ac.uk>
2022-08-16 15:05:38 +0100

    Use biomaterial_id if biomaterial_name is not present

--- converter/biosamples.py
+++ converter/biosamples.py
@@ -6,7 +6,6 @@ from json_converter.json_mapper import JsonMapper
 from . import get_concrete_type
 from .errors import MissingBioSamplesDomain
 
-
 ATTRIBUTE_SPEC = {
     'Biomaterial Core - Biomaterial Id': ['content.biomaterial_core.biomaterial_id'],
     'HCA Biomaterial Type': ['content.describedBy', get_concrete_type],
@@ -31,11 +30,14 @@ class BioSamplesConverter:
         biomaterial_content = biomaterial.get('content', {})
         biomaterial_core = biomaterial_content.get('biomaterial_core', {})
         sample_accession = self.__define_accession(existing_accession, biomaterial_core)
+        biomaterial_name = biomaterial_core.get('biomaterial_name') if biomaterial_core.get('biomaterial_name') \
+            else biomaterial_core.get('biomaterial_id', '')
         sample = Sample(
             accession=sample_accession,
-            name=biomaterial_core.get('biomaterial_name'),
+            name=biomaterial_name,
             domain=domain if domain else self.default_domain,
-            species=biomaterial_content['genus_species'][0].get('ontology_label') if biomaterial_content.get('genus_species') else None,
+            species=biomaterial_content['genus_species'][0].get('ontology_label') if biomaterial_content.get(
+                'genus_species') else None,
             ncbi_taxon_id=biomaterial_core.get('ncbi_taxon_id')[0] if biomaterial_core.get('ncbi_taxon_id') else None,
             update=self.__convert_datetime(biomaterial.get('updateDate')),
             release=self.__convert_datetime(release_date if release_date else biomaterial.get('submissionDate'))
@@ -46,14 +48,13 @@ class BioSamplesConverter:
 
     @staticmethod
     def __define_accession(existing_accession: str, biomaterial_core):
-        accession = existing_accession if existing_accession and len(existing_accession) > 0\
+        accession = existing_accession if existing_accession and len(existing_accession) > 0 \
             else biomaterial_core.get('biosamples_accession')
 
         if accession and len(accession) > 0:
             return accession
         else:
             return None
-
 
     @staticmethod
     def __add_attributes(sample: Sample, biomaterial: dict):
